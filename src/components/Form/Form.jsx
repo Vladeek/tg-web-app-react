46d1e9aea86affe75b95b668b502ac92c6584074
Vladeek <vlad.berinchikoof@gmail.com>
2022-11-23 22:08:57 +0300

    Adding config file for netlify

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
 import './Form.css'
 
 const Form = () => {
 	const [country, setCountry] = useState();
 	const [street, setStreet] = useState();
 	const [subject, setSubject] = useState('physical');
+	const {tg} = useTelegram();
+
+	useEffect(()=>{
+		tg.MainButton.setParams({
+			text: 'Отправить данные',
+		})
+	})
+
+	useEffect(()=>{
+		if(!street || !country){
+			tg.MainButton.hide()
+		}else{
+			tg.MainButton.show()
+		}
+	}, [country, street])
+
 
 	const onChangeCountry = (e) =>{
 		setCountry(e.target.value);
